1f2f9494ffb0535612d1ffd3cbe597d5f32d986c
laurin42 <laurin.schmidt@yahoo.com>
2025-10-23 21:10:03 +0200

    fixImageUrl fix

--- src/lib/posts/fixImageUrl.ts
+++ src/lib/posts/fixImageUrl.ts
@@ -1,9 +1,21 @@
 export function fixImageUrl(url?: string | null): string | null {
   if (!url) return null;
 
+  let fixedUrl: string;
   try {
-    return new URL(url, process.env.NEXT_PUBLIC_SITE_URL).toString().replace("-2368", "");
+    fixedUrl = new URL(url, process.env.NEXT_PUBLIC_SITE_URL).toString();
   } catch {
     return null;
   }
+  
+
+  const extRegex = /\.(png|jpe?g)$/i;
+  
+  if (extRegex.test(fixedUrl)) {
+      fixedUrl = fixedUrl.replace(extRegex, '.webp');
+  }
+
+  fixedUrl = fixedUrl.replace("-2368", ""); 
+  
+  return fixedUrl;
 }
